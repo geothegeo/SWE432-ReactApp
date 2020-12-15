effc925abdb49d9a76d39943912c7f1565ca33ac
George Tang <georgethewebmaster@gmail.com>
2020-12-15 15:56:10 -0600

    base template for final

---
+++ src/App (main).js
@@ -0,0 +1,59 @@
+import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+import './App.css';
+// import Fetcher from './components/Fetcher';
+// import Hooks, {aFunc} from './components/Hooks';
+// import ToggleButtons from './components/ToggleButtons';
+import RandomString from './components/RandomString';
+
+const publicURL = 'https://swe432tomcat.herokuapp.com';
+export const getLocationUrlData = () => {
+  return {
+      url:
+          process.env.NODE_ENV === 'production'?
+          publicURL
+          :`${window.location.origin}`,
+      hash: `${window.location.hash}`
+  };
+};
+export const servicePath ='/echo';
+
+function App(props) {
+  // const [weekDay, setWeekDay] = React.useState("Monday");
+  return (
+    <div style={{flexGrow: 1}}>
+      <Grid 
+      container
+      direction="column"
+      justify="center"
+      alignItems="stretch"
+      spacing={2}
+      >
+        <Grid item xs>
+          <Paper elevation={1}>
+            <RandomString />
+          </Paper>
+        </Grid>
+        {/* <Grid item xs>
+          <Paper elevation={1}>
+            <Hooks name={aFunc().name}/>
+            </Paper>
+        </Grid>
+        <Grid item xs>
+          <Paper elevation={1}>
+            <Fetcher  value={weekDay} url={`${getLocationUrlData().url}${servicePath}`}/>
+            </Paper>
+          </Grid>
+        <Grid item xs>
+          <Paper elevation={1}>
+            <ToggleButtons value={weekDay} onChange ={setWeekDay}/>
+          </Paper>
+          </Grid> */}
+      </Grid>
+    </div>
+  );
+}
+
+export default App;
--- src/App.js
+++ src/App.js
@@ -1,57 +1,104 @@
 import React from 'react';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-
 import './App.css';
-// import Fetcher from './components/Fetcher';
-// import Hooks, {aFunc} from './components/Hooks';
-// import ToggleButtons from './components/ToggleButtons';
-import RandomString from './components/RandomString';
+import { Button, Grid, TextField } from '@material-ui/core';
+import { ToggleButton, ToggleButtonGroup } from '@material-ui/lab';
 
-const publicURL = 'https://swe432tomcat.herokuapp.com';
-export const getLocationUrlData = () => {
-  return {
-      url:
-          process.env.NODE_ENV === 'production'?
-          publicURL
-          :`${window.location.origin}`,
-      hash: `${window.location.hash}`
-  };
-};
-export const servicePath ='/echo';
+function App() {
+  const [inputStrings, setInputStrings] = React.useState("");
+  const [outputStrings, setOutputStrings] = React.useState("");
+  const [replacement, setReplacement] = React.useState(true);
+  const handleInputChange = (event) => {
+    setInputStrings(event.target.value);
+  }
+  const handleReplacement = () => {
+    setReplacement(!replacement);
+  }
 
-function App(props) {
-  // const [weekDay, setWeekDay] = React.useState("Monday");
+  const chooseString = () => {
+    const strings = inputStrings.split("\n");
+    const chosen = strings[Math.floor(Math.random() * Math.floor(strings.length))];
+    if(inputStrings === "") {
+      return;
+    }
+    if(outputStrings === "") {
+      setOutputStrings(chosen);
+    }
+    else {
+      setOutputStrings(outputStrings + "\n" + chosen);
+    }
+    if(!replacement) {
+      const index = strings.indexOf(chosen);
+      if (index > -1) {
+        strings.splice(index, 1);
+      }
+      setInputStrings(strings.toString().replaceAll(",", "\n"));
+    }
+  }
+  const clearAll = async () => {
+    setInputStrings("");
+    setOutputStrings("");
+  }
+
   return (
-    <div style={{flexGrow: 1}}>
-      <Grid 
-      container
-      direction="column"
-      justify="center"
-      alignItems="stretch"
-      spacing={2}
-      >
+    <div className="App" style={{margin: '3em 10%', flexGrow: 1}}>
+      <Grid container justify="center">
         <Grid item xs>
-          <Paper elevation={1}>
-            <RandomString />
-          </Paper>
+          <TextField 
+            label="Strings to Be Chosen" 
+            variant="outlined" 
+            multiline 
+            rows={20}
+            value={inputStrings}
+            onChange={handleInputChange}
+          >
+
+          </TextField>
         </Grid>
-        {/* <Grid item xs>
-          <Paper elevation={1}>
-            <Hooks name={aFunc().name}/>
-            </Paper>
+        <Grid item container xs direction="column" justify="center" alignItems="center">
+          <p><b>How This Works:</b> <br/>Enter any number of strings into the left text box, separating them
+          with new lines. Your randomly chosen string will be added to the text box on the right.
+          <br/>[WITH REPLACEMENT] will keep the randomly chosen string in your list.
+          <br/>[WITHOUT REPLACEMENT] will remove the randomly chosen string from your list. <hr /></p>
+          
+          <ToggleButtonGroup 
+            value={replacement}
+            exclusive
+            onChange={handleReplacement}
+          >
+            <ToggleButton value={true}> With Replacement </ToggleButton>
+            <ToggleButton value={false}> Without Replacement </ToggleButton>
+          </ToggleButtonGroup>
+          <Button 
+            variant="contained"
+            color="primary"
+            disableElevation
+            onClick={chooseString}>
+            Choose A Random String!
+          </Button>
+          <Button 
+            variant="contained"
+            color="secondary"
+            disableElevation
+            onClick={clearAll}>
+            Clear All Strings!
+          </Button>
         </Grid>
         <Grid item xs>
-          <Paper elevation={1}>
-            <Fetcher  value={weekDay} url={`${getLocationUrlData().url}${servicePath}`}/>
-            </Paper>
-          </Grid>
-        <Grid item xs>
-          <Paper elevation={1}>
-            <ToggleButtons value={weekDay} onChange ={setWeekDay}/>
-          </Paper>
-          </Grid> */}
+          <TextField 
+            label="Chosen Strings" 
+            variant="outlined" 
+            multiline 
+            rows={20}
+            value={outputStrings}
+            InputProps={{
+              readOnly: true,
+            }}
+          >
+
+          </TextField>
+        </Grid>
       </Grid>
+      
     </div>
   );
 }
